f71fdcf36fa2c99d8339283faf73b204399e4ed7
d-mitrofanov-v <dmitriy.mitrofanov.v@gmail.com>
2025-05-17 18:30:15 +0300

    add last games

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -1,1 +1,33 @@
 <?php
+
+namespace Hexlet\Code\Games\Prime;
+
+use function Hexlet\Code\runEngine;
+
+const GAME_DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+function isPrime(int $num): bool
+{
+    if ($num === 1) {
+        return false;
+    }
+    for ($i = 2; $i <= $num / 2; $i++) {
+        if ($num % $i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function run(): void
+{
+    $getQuestionValue = function () {
+        return random_int(1, 50);
+    };
+
+    $getAnswer = function ($num) {
+        return isPrime((int)$num);
+    };
+
+    runEngine(GAME_DESCRIPTION, $getQuestionValue, $getAnswer);
+}
--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -1,1 +1,38 @@
 <?php
+
+namespace Hexlet\Code\Games\Progression;
+
+use function Hexlet\Code\runEngine;
+
+const GAME_DESCRIPTION = 'What number is missing in the progression?';
+
+function run(): void
+{
+    $getQuestionValue = function () {
+        $num1 = rand(1, 50);
+        $progressionLen = rand(5, 11);
+        $progressionStep = rand(1, 20);
+        $indexToHide = rand(0, $progressionLen - 1);
+        $progression = [$num1];
+
+        for ($i = 0; $i < $progressionLen; $i++) {
+            $lastItem = $progression[count($progression) - 1];
+            $progression[] = $lastItem + $progressionStep;
+        }
+
+        $progression[$indexToHide] = "..";
+        return implode(" ", $progression);
+    };
+
+    $getAnswer = function ($progression) {
+        $numbers = explode(" ", $progression);
+        $hiddenIndex = array_search("..", $numbers);
+        $num1 = (int)$numbers[$hiddenIndex - 1];
+        $num2 = (int)$numbers[$hiddenIndex - 2];
+        $step = $num1 - $num2;
+        $answer = (int)$numbers[$hiddenIndex + 1] - $step;
+        return (string)$answer;
+    };
+
+    runEngine(GAME_DESCRIPTION, $getQuestionValue, $getAnswer);
+}
